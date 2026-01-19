9efbb38fa85b07ac8df7b2e24504166a42fdb72e
Maxlagartilloia <maxlagartilloia@gmail.com>
2026-01-19 18:24:09 -0500

    Delete js/password-toggle.js

--- js/password-toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("togglePassword");
-  const input = document.getElementById("password");
-
-  toggle.addEventListener("click", () => {
-    input.type = input.type === "password" ? "text" : "password";
-  });
-});
